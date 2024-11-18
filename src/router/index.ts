12ae740dd511691418527ebcde9da451bd8c2df8
ES0TERIX <thomas.carre74@gmail.com>
2024-11-18 17:58:41 +0100

    [TD1] Part 1,2,3 - (TODO exo 2,3)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../presentation/views/HomeView.vue';
+import ParcoursListView from '@/presentation/views/ParcoursListView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/parcours',
+      name: 'parcours',
+      component: ParcoursListView
     }
   ]
 });
